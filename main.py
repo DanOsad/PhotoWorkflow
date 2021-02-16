d4084ae5e30c21cc4ef80fa6452e8c614e709665
Dan Osadtsuk <dan@osadtsuk.com>
2021-02-15 21:05:04 -0500

    added some functions for creating Clients

--- main.py
+++ main.py
@@ -2,4 +2,17 @@ from models import *
 
 # MAIN APP LOGIC HERE
 
-print(models.user_id())
+def user_id():
+    
+    user_id = int(r.randint(1000000,10000000))
+    while user_id in user_ids:
+        user_id = int(r.randint(1000000,10000000))
+    user_ids.append(user_id)
+    return user_id
+
+def create_user(firstName, lastName):
+    clientId = user_id()
+    newUser = client.Client(firstName, lastName, clientId)
+    print(f"{firstName} {lastName}'s user id is {clientId}")
+
+# if __name__ == "__main__"
